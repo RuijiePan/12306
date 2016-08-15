7b1c60b0e70eadfe039f2b317b65e40bceebd0e7
prj <zquprj@gmail.com>
2016-08-15 23:44:47 +0800

    第一次上传

---
+++ my12306/app/src/main/java/ruijie/com/my12306/MyApplication.java
@@ -0,0 +1,15 @@
+package ruijie.com.my12306;
+
+import android.app.Application;
+
+/**
+ * Created by prj on 2016/8/15.
+ */
+
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ my12306/app/src/main/java/ruijie/com/my12306/injector/component/ApplicationComponent.java
@@ -0,0 +1,9 @@
+package ruijie.com.my12306.injector.component;
+
+/**
+ * Created by prj on 2016/8/15.
+ */
+
+public interface ApplicationComponent {
+
+}
